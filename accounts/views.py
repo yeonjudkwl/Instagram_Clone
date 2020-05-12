2431dc386f487755f60929b5ae09c0ede28d8603
yeonjudkwl <hyj9785@likelion.org>
2020-05-12 13:29:37 +0000

    login

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login as auth_login
 from .forms import CustomUserCreationForm
 
 # Create your views here.
@@ -6,12 +8,25 @@ def signup(request):
     if request.method == 'POST':
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
-            # 로그인 후 메인페이지 리다이렉트 할 예정
-            return render(request, 'accounts/signup.html')
+            user = form.save()
+            auth_login(request, user)
+            return redirect('instagram:index')
     else:
         form = CustomUserCreationForm()
     context = {
         'form': form
     }
     return render(request, 'accounts/signup.html', context)
+
+def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            return redirect('instagram:index')
+    else:
+        form = AuthenticationForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'accounts/login.html', context)
